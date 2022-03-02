b8bd840397c5771a6a21f848b0f95d9ebb76fc44
Andrej Germič <andrej.germic@moderntv.eu>
2022-03-03 00:32:39 +0100

    Feat: fallback styles

--- src/components/Fallback.tsx
+++ src/components/Fallback.tsx
@@ -1,16 +1,40 @@
 import { FC, ReactNode } from "react"
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
+import { Refresh } from "@styled-icons/heroicons-solid"
 
 /**
  * Fallback component provides loading/error screen for components
  */
 
 const FallbackWrapper = styled.div`
+  color: white;
   width: 100%;
-  height: 100%;
-  font-size: 12rem;
+  margin-top: 30px;
+  font-size: 4rem;
   line-height: 100%;
   text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column-reverse;
+  @media screen and (min-width: 560px) {
+    flex-direction: row;
+  }
+`
+
+const loadAnimation = keyframes`
+ from { transform: rotate(0deg);}
+ to { transform: rotate(-360deg);}
+`
+
+const LoadIcon = styled(Refresh)`
+  animation: ${loadAnimation} 1s linear infinite;
+  margin: 20px 0;
+  width: 48px;
+  @media screen and (min-width: 560px) {
+    margin: 0;
+    margin-right: 20px;
+  }
 `
 
 interface Props {
@@ -20,7 +44,13 @@ interface Props {
 }
 
 const Fallback: FC<Props> = ({ loading, error, children }) => {
-  if (loading) return <FallbackWrapper>Loading...</FallbackWrapper>
+  if (loading)
+    return (
+      <FallbackWrapper>
+        <LoadIcon />
+        Loading...
+      </FallbackWrapper>
+    )
 
   if (error) return <FallbackWrapper>Error</FallbackWrapper>
 
