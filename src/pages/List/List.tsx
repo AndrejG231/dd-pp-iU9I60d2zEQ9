c24d54e5490e81d155e211d0d8302e443cc882d9
Andrej Germič <andrej.germic@moderntv.eu>
2022-03-02 22:53:03 +0100

    feat: search products

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,5 +1,6 @@
-import { useCallback } from "react"
+import { ChangeEventHandler, useCallback, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
 import { useProductsData } from "../../api/products-data"
 import Fallback from "../../components/Fallback"
 import { ProductCard } from "../../components/ProductCard"
@@ -7,8 +8,10 @@ import { addToCart, removeFromCart } from "../../store/actions/manage-cart"
 import { State } from "../../store/store"
 
 const List = () => {
-  const { products, loading, error } = useProductsData()
   const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const { products, loading, error } = useProductsData()
   const counts = useSelector((state: State) => state.cart.counts)
 
   // Cart actions
@@ -21,19 +24,34 @@ const List = () => {
     [dispatch]
   )
 
+  // Search string
+  const [searchStr, setSearchStr] = useState("")
+
+  const handleSearch = useCallback<ChangeEventHandler<HTMLInputElement>>(
+    ({ target: { value } }) => setSearchStr(value),
+    [setSearchStr]
+  )
+
   return (
     <div>
+      <input type="text" value={searchStr} onChange={handleSearch} />
+      <button onClick={() => navigate("/cart")}>Shopping Cart</button>
+      {/* Display loading/error screen */}
       <Fallback loading={loading} error={error}>
-        {Object.values(products).map(({ id, name, price }) => (
-          <ProductCard
-            key={id}
-            name={name}
-            price={price}
-            count={~~counts[id]}
-            onPlusClick={() => increaseInCart(id)}
-            onMinusClick={() => decreaseInCart(id)}
-          />
-        ))}
+        {Object.values(products).map(({ id, name, price }) => {
+          // Filter out unmatched
+          if (!name.includes(searchStr)) return null
+          return (
+            <ProductCard
+              key={id}
+              name={name}
+              price={price}
+              count={~~counts[id]}
+              onPlusClick={() => increaseInCart(id)}
+              onMinusClick={() => decreaseInCart(id)}
+            />
+          )
+        })}
       </Fallback>
     </div>
   )
